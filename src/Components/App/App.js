3e39404e1d269ba97afefda5d6ab7964a964cfc0
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-21 01:33:46 -0400

    Get sucessful fetch call in TodaysReading.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,25 +3,9 @@ import NavBar from '../NavBar/NavBar';
 import AllSigns from '../AllSigns/AllSigns';
 import TodaysReading from '../TodaysReading/TodaysReading';
 import { useEffect, useState } from 'react';
-import getData from '../../apiCalls';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
-
-  // useEffect(() => {
-  //   const apiCall = async() => {
-  //     try {
-  //       const data = await getData('https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=aries&timePeriod=yesterday')
-  //       console.log(data, 'this is data testing')
-  //     } catch(error) {
-  //       if(error instanceof Error) {
-  //         console.log('there was an error')
-  //       }
-  //     }
-  //   }
-  //   apiCall()
-  // }, [])
 
   return (
     <div className="App">
--- src/Components/TodaysReading/TodaysReading.js
+++ src/Components/TodaysReading/TodaysReading.js
@@ -1,15 +1,28 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import getData from '../../apiCalls';
 
 const TodaysReading = () => {
-  const sign = useParams();
-
+  
   const [selectedSign, setSelectedSign] = useState('')
-
-
-  useEffect(() => {
+  const sign = useParams();
+  
+  useEffect((selectedSign) => {
     setSelectedSign(sign)
-  })
+    console.log(sign, 'selected sign inside fetch call')
+    const apiCall = async(selectedSign) => {
+      try {
+        const data = await getData(`https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=scorpio&timePeriod=today`)
+        console.log(data, 'this is data testing')
+        console.log(sign, 'omfg')
+      } catch(error) {
+        if(error instanceof Error) {
+          console.log('there was an error')
+        }
+      }
+    }
+    apiCall(selectedSign)
+  }, [])
 
   console.log(selectedSign, 'this is selected sign')
 
