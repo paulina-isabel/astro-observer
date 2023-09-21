df8bb0ecfeed58d81214f95ed385107ccc67ce45
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-20 22:09:24 -0400

    Add dummy signs.

---
+++ src/Components/AllSigns/AllSigns.js
@@ -0,0 +1,44 @@
+import aries from '../../images/aries.png';
+import taurus from '../../images/taurus.png';
+import gemini from '../../images/gemini.png';
+import cancer from '../../images/cancer.png';
+import leo from '../../images/leo.png';
+import virgo from '../../images/virgo.png';
+import libra from '../../images/libra.png';
+import scorpio from '../../images/scorpio.png';
+import sagittarius from '../../images/sagittarius.png';
+import capricorn from '../../images/capricorn.png';
+import aquarius from '../../images/aquarius.png';
+import pisces from '../../images/pisces.png';
+
+const AllSigns = () => {
+
+  const allSigns = [
+    {
+      sign: 'aries',
+      image: aries,
+      id: 1
+    },
+    {
+      sign: 'taurus',
+      image: taurus,
+      id: 2
+    },
+    {
+      sign: 'gemini',
+      image: gemini,
+      id: 3
+    },
+
+    {
+      sign: 'cancer',
+      image: cancer,
+      id: 4
+    }
+    //  'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'
+  ]
+
+
+}
+
+export default AllSigns
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,27 +1,30 @@
 import './App.css';
 import NavBar from '../NavBar/NavBar';
+import AllSigns from '../AllSigns/AllSigns';
 import { useEffect, useState } from 'react';
 import getData from '../../apiCalls';
 
+
 function App() {
 
-  useEffect(() => {
-    const apiCall = async() => {
-      try {
-        const data = await getData('https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=aries&timePeriod=yesterday')
-        console.log(data, 'this is data testing')
-      } catch(error) {
-        if(error instanceof Error) {
-          console.log('there was an error')
-        }
-      }
-    }
-    apiCall()
-  }, [])
+  // useEffect(() => {
+  //   const apiCall = async() => {
+  //     try {
+  //       const data = await getData('https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=aries&timePeriod=yesterday')
+  //       console.log(data, 'this is data testing')
+  //     } catch(error) {
+  //       if(error instanceof Error) {
+  //         console.log('there was an error')
+  //       }
+  //     }
+  //   }
+  //   apiCall()
+  // }, [])
 
   return (
     <div className="App">
       <NavBar />
+      <AllSigns />
     </div>
   );
 }
