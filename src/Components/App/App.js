1719c99503a55734c515e9753b9584bda3914b5f
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-09-21 20:26:11 -0400

    Add reading to local storage.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,12 +7,26 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
+  const [favoriteReadings, setFavoriteReadings] = useState([])
+
+  const addToFavorites = (newReading) => {
+    window.localStorage.setItem('favoriteReadings', JSON.stringify([...favoriteReadings, newReading]))
+    setFavoriteReadings(JSON.parse(localStorage.favoriteReadings))
+  
+  }
+
+  // const removeFromFavorites = (reading) => {
+  //   const filteredProducts = savedProducts.filter(oldProduct => oldProduct.id !== product.id)
+  //   window.localStorage.setItem('favoriteReadings', JSON.stringify(filteredProducts))
+  //   setSavedProducts(JSON.parse(localStorage.savedProducts))
+  // }
+
   return (
     <div className="App">
       <NavBar />
       <Routes>
         <Route path='/' element={<AllSigns />} />
-        <Route path='/:sign' element={<TodaysReading />} />
+        <Route path='/:sign' element={<TodaysReading addToFavorites={addToFavorites} favoriteReadings={favoriteReadings}/>} />
       </Routes>
     </div>
   )
--- src/Components/TodaysReading/TodaysReading.js
+++ src/Components/TodaysReading/TodaysReading.js
@@ -4,29 +4,33 @@ import { useEffect, useState } from 'react';
 import today from '../.././images/today.png';
 import yesterday from '../.././images/yesterday.png';
 import weekly from '../.././images/weekly.png';
+import favorite from '../.././images/favorite.png';
+import unfavorite from '../.././images/unfavorite.png';
 import getData from '../../apiCalls';
 
-const TodaysReading = () => {
+const TodaysReading = ({ addToFavorites, favoriteReadings }) => {
   const [selectedTimePeriod, setSelectedTimePeriod] = useState('');
-  const [prediction, setPrediction] = useState('')
+  const [reading, setReading] = useState('this is a test')
   
   const { sign } = useParams();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getData(`https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=${sign}&timePeriod=${selectedTimePeriod}`);
-        console.log(data, 'this is data testing');
-        setPrediction(data.prediction)
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log('there was an error');
-        }
-      }
-    };
+  console.log(favoriteReadings, 'this is FAVORITE READINGS')
 
-    fetchData();
-  }, [selectedTimePeriod]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await getData(`https://daily-horoscope-api.p.rapidapi.com/api/Daily-Horoscope-English/?zodiacSign=${sign}&timePeriod=${selectedTimePeriod}`);
+  //       console.log(data.prediction, 'this is data testing');
+  //       setReading(data.prediction)
+  //     } catch (error) {
+  //       if (error instanceof Error) {
+  //         console.log('there was an error');
+  //       }
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [selectedTimePeriod]);
 
   return (
     <div className='todays-reading'>
@@ -58,9 +62,13 @@ const TodaysReading = () => {
       <h3>
         {selectedTimePeriod}
       </h3>
-      <p className='prediction'>
-        {prediction}
-      </p>
+      <div className='reading'>
+        <p>
+          Your friendly, kind and noble nature brings people close to you. You won't have to take any great efforts to do this.The love and joy that you would see in your partner's eyes would make toda
+          {reading}
+        </p>
+        <img src={favorite} className='favorite-button' onClick={() => addToFavorites(reading)}/>
+      </div>
 
     </div>
   )
